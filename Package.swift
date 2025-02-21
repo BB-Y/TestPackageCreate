d0df5d23fccdb0c941886924eb1b22d38753604f
huangzhenxin <zhenxin924@qq.com>
2025-02-21 16:01:01 +0800

    init

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MyTestLibrary",
+    platforms:[
+        .macOS(.v11), .iOS(.v14),.tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/MyTestLibrary/MyTestLibrary.swift
+++ Sources/MyTestLibrary/MyTestLibrary.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    TestView()
+}
